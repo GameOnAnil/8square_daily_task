b1633f5b36a9f7c5ca35b74374d089150c59a82f
anil thapa <gameonanil@gmail.com>
2022-07-01 14:18:41 +0545

    git fix

--- intern_task_week2/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/arm64/path_provider_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/anilthapa/Flutter\ Project/intern_task_week2/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
-  /Users/anilthapa/Flutter\ Project/intern_task_week2/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
-  /Users/anilthapa/Flutter\ Project/intern_task_week2/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
